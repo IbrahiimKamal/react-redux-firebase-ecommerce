b2fb8e7f36a2aa69e65328d11119c9ce519e8a4a
Ibrahim Kamal <ebrahiiim.kamal@gmail.com>
2021-07-27 20:11:04 +0200

    modified firebase config file

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyAWNkRCt217tNTV1mO8K_PY8Krr5dfCsik',
+  authDomain: 'ecommercy1.firebaseapp.com',
+  projectId: 'ecommercy1',
+  storageBucket: 'ecommercy1.appspot.com',
+  messagingSenderId: '737493530936',
+  appId: '1:737493530936:web:9b30e4eff8eed4132ae9cd',
 };
 
 firebase.initializeApp(firebaseConfig);
